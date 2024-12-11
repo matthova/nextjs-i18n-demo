2815fdc33ea627a2223d2aec443f4b4b433195c5
Matt Hova <matthova87@gmail.com>
2024-12-11 12:25:32 -0800

    Add github link

--- src/app/[locale]/(app)/page.tsx
+++ src/app/[locale]/(app)/page.tsx
@@ -1,14 +1,28 @@
-import { useTranslations } from 'next-intl';
-import { Link } from '@/i18n/routing';
+import { useTranslations } from "next-intl";
+import { Link } from "@/i18n/routing";
+
+const GITHUB_REPO_LINK = "https://github.com/matthova/nextjs-i18n-demo";
 
 export default function HomePage() {
-    const t = useTranslations('HomePage');
+  const t = useTranslations("HomePage");
 
-    return (
-        <div>
-            <h1>{t('title')}</h1>
-            <Link href="/about">{t('about')}</Link>
-        </div>
-    );
+  return (
+    <div>
+      <h1>{t("title")}</h1>
+      <Link href="/about">{t("about")}</Link>
+      <div>
+        {t.rich("github", {
+          link: (chunks) => (
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href={GITHUB_REPO_LINK}
+            >
+              {chunks}
+            </a>
+          ),
+        })}
+      </div>
+    </div>
+  );
 }
-

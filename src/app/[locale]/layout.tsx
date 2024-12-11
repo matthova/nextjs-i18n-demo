a7ac0e77f16f00f5bec7e75cdef983cae46a0253
Matt Hova <matthova87@gmail.com>
2024-12-11 12:12:28 -0800

    casting fun

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,34 +1,34 @@
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-import { notFound } from 'next/navigation';
-import { routing } from '@/i18n/routing';
-import '../global.css';
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import "../global.css";
 
 export default async function LocaleLayout({
-    children,
-    params
+  children,
+  params,
 }: {
-    children: React.ReactNode;
-    params: Promise<{ locale: string }>;
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
-    const { locale } = await params
-    // Ensure that the incoming `locale` is valid
-    if (!routing.locales.includes(locale as any)) {
-        notFound();
-    }
+  const { locale } = await params;
+  // Ensure that the incoming `locale` is valid
+  // @ts-expect-error string to array entry
+  if (!routing.locales.includes(locale as unknown)) {
+    notFound();
+  }
 
-    // Providing all messages to the client
-    // side is the easiest way to get started
-    const messages = await getMessages();
+  // Providing all messages to the client
+  // side is the easiest way to get started
+  const messages = await getMessages();
 
-    return (
-        <html lang={locale} className="m-0 p-0 h-full">
-            <body className="m-0 p-0 h-full">
-                <NextIntlClientProvider messages={messages}>
-                    {children}
-                </NextIntlClientProvider>
-            </body>
-        </html>
-    );
+  return (
+    <html lang={locale} className="m-0 p-0 h-full">
+      <body className="m-0 p-0 h-full">
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
-
